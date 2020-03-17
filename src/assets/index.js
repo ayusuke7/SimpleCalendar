f4b770f66a0cae6144c1a30ece4a766587e7d5f6
ayusuke7 <alexhcb.7392@gmail.com>
2020-03-17 00:31:25 -0300

    publish

--- src/assets/index.js
+++
@@ -1,4 +0,0 @@
-import chevronLeft from "../assets/chevron-left.svg";
-import chevronRight from "../assets/chevron-right.svg";
-
-export { chevronLeft, chevronRight };
--- src/index.js
+++ src/index.js
@@ -1,30 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import SimpleCalendar from "./components/SimpleCalendar";
-import Symbols, { getAll } from "./utils/Symbols";
-
-const dates = () => {
-  return getAll().map((s, i) =>
-    Object({
-      date: `2020-03-0${i + 1}`,
-      icon: s,
-      iconSize: "20px"
-    })
-  );
-};
 
 ReactDOM.render(
   <SimpleCalendar
     //locale="pt"
-    dates={dates()}
+    //dates={dates()}
     //initDate={"2020-12-31"}
     //weekNamesAbrv
     enableSelectDays
     //enableSelectDateNow
     //customWeekNames={["A", "B", "C", "D", "E", "F", "G"]}
-    onClickPrev={data => console.log(data)}
-    onClickNext={data => console.log(data)}
-    onClickDay={data => console.log(data)}
+    //onClickPrev={data => console.log(data)}
+    //onClickNext={data => console.log(data)}
+    //onClickDay={data => console.log(data)}
   />,
   document.getElementById("root")
 );
